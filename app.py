50f01cdbbe28c4315a3520444348a3e748c6cf95
Wellington Arantes <w.aranteslfc@gmail.com>
2025-01-23 19:02:08 -0300

    feat: add view cart functionality with API endpoint

--- app.py
+++ app.py
@@ -151,5 +151,22 @@ def remove_from_cart(product_id):
         return jsonify({'message': 'Product removed successfully'})
     return jsonify({'message': 'Failed to remove product from the cart'}), 400
 
+@app.route('/api/cart', methods=['GET'])
+@login_required
+def view_cart():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    cart_content = []
+    for cart_item in cart_items:
+        product = Product.query.get(cart_item.product_id)
+        cart_content.append({
+            'id': cart_item.id,
+            'user_id': cart_item.user_id,
+            'product_id': cart_item.product_id,
+            'product_name': product.name,
+            'product_price': product.price
+        })
+    return jsonify(cart_content)
+
 if __name__ == '__main__':
   app.run(debug=True)
